ed136960ed4bbee67b5f4918d6232c98f1b41f6c
Anders Norrback Bornholm <anders@bornholm.se>
2017-10-24 22:09:41 +0200

    Bypassed auth (for now). Added house routes.

--- migrations/20160420191812-createTables.js
+++ migrations/20160420191812-createTables.js
@@ -2,6 +2,7 @@ exports.up = function (r, connection) {
   return Promise.all([
     r.tableCreate('users').run(connection),
     r.tableCreate('userTokens').run(connection),
+    r.tableCreate('houses').run(connection),
     r.tableCreate('rooms').run(connection),
     r.tableCreate('plants').run(connection)
   ]);
@@ -11,8 +12,8 @@ exports.down = function (r, connection) {
   return Promise.all([
     r.tableDrop('users').run(connection),
     r.tableDrop('userTokens').run(connection),
+    r.tableDrop('houses').run(connection),
     r.tableDrop('rooms').run(connection),
     r.tableDrop('plants').run(connection)
   ]);
 };
-
--- migrations/20160420202204-seedData.js
+++ migrations/20160420202204-seedData.js
@@ -5,14 +5,19 @@ exports.up = function (r, connection) {
       name: 'Anders',
       email: 'anders@bornholm.se'
     }).run(connection),
+    r.table('houses').insert({
+      id: '8d76e546-ce33-489b-af74-4a6979e0124a',
+      userId: '8bdd0c6c-a9f4-4255-ab92-c5f52d74bb2a',
+      name: 'Rättaregården'
+    }).run(connection),
     r.table('rooms').insert({
       id: '8d76e546-ce33-489b-af74-4a6979e0113a',
-      userId: '8bdd0c6c-a9f4-4255-ab92-c5f52d74bb2a',
+      houseId: '8d76e546-ce33-489b-af74-4a6979e0124a',
       name: 'Vardagsrummet'
     }).run(connection),
     r.table('rooms').insert({
       id: '0c522884-b768-44b2-aaf8-47d667137203',
-      userId: '8bdd0c6c-a9f4-4255-ab92-c5f52d74bb2a',
+      houseId: '8d76e546-ce33-489b-af74-4a6979e0124a',
       name: 'Matsalen'
     }).run(connection),
     r.table('plants').insert({
@@ -22,7 +27,7 @@ exports.up = function (r, connection) {
       waterInterval: 4,
       lastWatered: new Date(),
       imageUrl: 'http://loremflickr.com/640/640/pottedplant'
-    }),
+    }).run(connection),
     r.table('plants').insert({
       id: 'd664fae7-3ea0-4afe-89d4-dce1c4d2e126',
       roomId: '0c522884-b768-44b2-aaf8-47d667137203',
@@ -30,7 +35,7 @@ exports.up = function (r, connection) {
       waterInterval: 21,
       lastWatered: new Date(),
       imageUrl: 'http://loremflickr.com/640/640/pottedplant'
-    }),
+    }).run(connection),
     r.table('plants').insert({
       id: 'd664fae7-3ea0-4afe-89d4-dce1c4d2e8ef',
       roomId: '0c522884-b768-44b2-aaf8-47d667137203',
@@ -38,7 +43,7 @@ exports.up = function (r, connection) {
       waterInterval: 7,
       lastWatered: new Date(),
       imageUrl: 'http://loremflickr.com/640/640/pottedplant'
-    }),
+    }).run(connection),
     r.table('plants').insert({
       id: 'd664fae7-3ea0-4afe-89d4-dce1c4d2e124',
       roomId: '0c522884-b768-44b2-aaf8-47d667137203',
@@ -46,7 +51,7 @@ exports.up = function (r, connection) {
       waterInterval: 7,
       lastWatered: new Date(),
       imageUrl: 'http://loremflickr.com/640/640/pottedplant'
-    }),
+    }).run(connection),
     r.table('plants').insert({
       id: 'd664fae7-3ea0-4afe-89d4-dce1c4d2e125',
       roomId: '0c522884-b768-44b2-aaf8-47d667137203',
@@ -54,7 +59,7 @@ exports.up = function (r, connection) {
       waterInterval: 7,
       lastWatered: new Date(),
       imageUrl: 'http://loremflickr.com/640/640/pottedplant'
-    }),
+    }).run(connection),
     r.table('plants').insert({
       id: 'd664fae7-3ea0-4afe-89d4-dce1c4d2e123',
       roomId: '0c522884-b768-44b2-aaf8-47d667137203',
@@ -68,7 +73,7 @@ exports.up = function (r, connection) {
 
 exports.down = function (r, connection) {
   return Promise.all([
-    r.table('users').get('8bdd0c6c-a9f4-4255-ab92-c5f52d74bb2a').delete().run(connection),
+    r.table('plants').get('8bdd0c6c-a9f4-4255-ab92-c5f52d74bb2a').delete().run(connection),
     r.table('instruments').get('8d76e546-ce33-489b-af74-4a6979e0113a').delete().run(connection),
     r.table('instruments').get('0c522884-b768-44b2-aaf8-47d667137203').delete().run(connection)
   ]);
